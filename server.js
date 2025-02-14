03fcaeb0441e86b8110d81f28bbfb968a4d1d8bb
IgorMan <136973037+IgorMan2005@users.noreply.github.com>
2025-02-14 14:06:06 +0300

    port 8000 (!)
    for https://amplified-rich-spice.glitch.me/students

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('data.json');
 const middlewares = jsonServer.defaults();
-const port = 8005;
+const port = 8000;
 
 server.use(middlewares);
 server.use(router);
